37c6fb56ef2dbd0b7103029eae7b2e8cf74f89a5
ghl <1519469195@qq.com>
2022-05-20 17:53:38 +0800

    lazy

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -10,77 +10,88 @@
 uint64
 sys_exit(void)
 {
-  int n;
-  if(argint(0, &n) < 0)
-    return -1;
-  exit(n);
-  return 0;  // not reached
+    int n;
+    if (argint(0, &n) < 0)
+        return -1;
+    exit(n);
+    return 0; // not reached
 }
 
 uint64
 sys_getpid(void)
 {
-  return myproc()->pid;
+    return myproc()->pid;
 }
 
 uint64
 sys_fork(void)
 {
-  return fork();
+    return fork();
 }
 
 uint64
 sys_wait(void)
 {
-  uint64 p;
-  if(argaddr(0, &p) < 0)
-    return -1;
-  return wait(p);
+    uint64 p;
+    if (argaddr(0, &p) < 0)
+        return -1;
+    return wait(p);
 }
 
 uint64
 sys_sbrk(void)
 {
-  int addr;
-  int n;
+    int addr;
+    int n;
 
-  if(argint(0, &n) < 0)
-    return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
-  return addr;
+    if (argint(0, &n) < 0)
+        return -1;
+    struct proc *p = myproc();
+    addr = p->sz;
+    if (n < 0)
+    {
+        if (p->sz + n < 0)
+            return -1;
+        else
+            uvmdealloc(p->pagetable, p->sz, p->sz + n);
+    }
+    p->sz += n;
+    //   if(growproc(n) < 0)
+    //     return -1;
+    return addr;
 }
 
 uint64
 sys_sleep(void)
 {
-  int n;
-  uint ticks0;
+    int n;
+    uint ticks0;
 
-  if(argint(0, &n) < 0)
-    return -1;
-  acquire(&tickslock);
-  ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
-      release(&tickslock);
-      return -1;
+    if (argint(0, &n) < 0)
+        return -1;
+    acquire(&tickslock);
+    ticks0 = ticks;
+    while (ticks - ticks0 < n)
+    {
+        if (myproc()->killed)
+        {
+            release(&tickslock);
+            return -1;
+        }
+        sleep(&ticks, &tickslock);
     }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
-  return 0;
+    release(&tickslock);
+    return 0;
 }
 
 uint64
 sys_kill(void)
 {
-  int pid;
+    int pid;
 
-  if(argint(0, &pid) < 0)
-    return -1;
-  return kill(pid);
+    if (argint(0, &pid) < 0)
+        return -1;
+    return kill(pid);
 }
 
 // return how many clock tick interrupts have occurred
@@ -88,10 +99,10 @@ sys_kill(void)
 uint64
 sys_uptime(void)
 {
-  uint xticks;
+    uint xticks;
 
-  acquire(&tickslock);
-  xticks = ticks;
-  release(&tickslock);
-  return xticks;
+    acquire(&tickslock);
+    xticks = ticks;
+    release(&tickslock);
+    return xticks;
 }
