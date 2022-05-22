52d6fa9d08c7e2ffe96664f757292497eb437979
ghl <1519469195@qq.com>
2022-05-22 20:18:09 +0800

    copy on write

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -8,58 +8,101 @@
 #include "spinlock.h"
 #include "riscv.h"
 #include "defs.h"
+#define PA2IDX(pa) (((uint64)pa) >> 12)
 
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
-  struct run *next;
+struct run
+{
+    struct run *next;
 };
 
-struct {
-  struct spinlock lock;
-  struct run *freelist;
+struct
+{
+    struct spinlock lock;
+    struct run *freelist;
 } kmem;
 
-void
-kinit()
+struct
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+    struct spinlock lock;
+    int count[PGROUNDUP(PHYSTOP) / PGSIZE];
+} refcnt;
+
+void rcinit()
+{
+    initlock(&refcnt.lock, "refcnt");
+    acquire(&kmem.lock);
+    for (int i = 0; i < PGROUNDUP(PHYSTOP) / PGSIZE; i++)
+        refcnt.count[i] = 0;
+    release(&kmem.lock);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void increase_rc(void *pa)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+    acquire(&refcnt.lock);
+    refcnt.count[PA2IDX(pa)]++;
+    release(&refcnt.lock);
+}
+
+void decrease_rc(void *pa)
+{
+    acquire(&refcnt.lock);
+    refcnt.count[PA2IDX(pa)]--;
+    release(&refcnt.lock);
+}
+int get_rc(void *pa)
+{
+    acquire(&refcnt.lock);
+    int rc = refcnt.count[PA2IDX(pa)];
+    release(&refcnt.lock);
+    return rc;
+}
+
+void kinit()
+{
+    rcinit();
+    initlock(&kmem.lock, "kmem");
+    freerange(end, (void *)PHYSTOP);
+}
+
+void freerange(void *pa_start, void *pa_end)
+{
+    char *p;
+    p = (char *)PGROUNDUP((uint64)pa_start);
+    for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+    {
+        increase_rc((void *)p);
+        kfree(p);
+    }
 }
 
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
-  struct run *r;
+    struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
+        panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    decrease_rc(pa);
+    if (get_rc(pa) > 0)
+        return;
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run *)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +111,18 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+    struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r = kmem.freelist;
+    if (r)
+        kmem.freelist = r->next;
+    release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    if (r)
+    {
+        memset((char *)r, 5, PGSIZE); // fill with junk
+        increase_rc((void *)r);
+    }
+    return (void *)r;
 }
